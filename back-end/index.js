f52f87adc82cbb12a6e1287fc7c31851cfbd629d
yongxialee leelianou <dejnaj@hotmail.com>
2023-08-18 10:10:22 -0600

    modified index.js file

--- back-end/index.js
+++ back-end/index.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('./lib/logger');
 var cors = require('cors');
-const host = ("RENDER" in process.env) ? `0.0.0.0` : `localhost`;
 
 var users = require('./routes/users');
 
@@ -46,9 +45,9 @@ app.use(function(err, req, res, next) {
 
 app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get({port:post,host:host}), function() {
+var server = app.listen(app.get('port'), function() {
   log.info(
-    'Express server listening on http://localhost:%d'||'host',
+    'Express server listening on http://localhost:%d',
     server.address().port
   );
 });
